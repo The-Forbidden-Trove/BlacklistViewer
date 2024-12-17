90d7f442b29a02f7af3fa1a44e9886960e5d6826
pacto <kuba.kowalski21331@gmail.com>
2024-12-17 12:46:54 +0100

    added gh link

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { GithubIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
@@ -6,11 +8,19 @@ export default function Footer() {
 
   return (
     <footer className="flex flex-col items-center justify-between gap-2 border-t bg-background px-4 py-4 sm:gap-0 md:flex-row">
-      {/* <div className="flex w-1/3 flex-wrap items-center justify-center gap-4 sm:justify-end"> */}
       <p className="text-center text-sm text-gray-500 dark:text-gray-400">
         {t("ggg")}
       </p>
-      {/* </div> */}
+
+      <Button size={"icon"} variant={"outline"} asChild>
+        <a
+          href="https://github.com/The-Forbidden-Trove/BlacklistViewer"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          <GithubIcon className="w-4 h-4" />
+        </a>
+      </Button>
     </footer>
   );
 }
